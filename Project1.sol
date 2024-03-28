08176a422ea92f8a3c28fdb80a3ca108a06f1bb8
8629070303ak <163749567+8629070303ak@users.noreply.github.com>
2024-03-28 19:35:41 +0530

    Update Project1.sol

--- Project1.sol
+++ Project1.sol
@@ -1,23 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.18;
 
-contract MyCoin {
+contract Ak_Token {
    
-  // public variables
-    string public tokenName = "META";
-    string public tokenAbbry = "HTA";
+  // public variables for token name and abbriviation
+    string public tokenName = "akshit";
+    string public tokenAbbry = "AKT";
     uint public totalSupply = 0;
     
  // mapping variable
     mapping(address => uint) public balances;
      
-// mint function
+// mint function TO MINT NEW TOKENS
     function mint (address _address, uint _value) public {
        totalSupply += _value;
        balances[_address] += _value;
     }
     
-// burn function
+// burn function to burn tokens
     function burn (address _address, uint _value) public {
        if (balances[_address] >= _value) {
           totalSupply -= _value;
